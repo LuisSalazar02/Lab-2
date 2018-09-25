06373159bec799879e292a50313663643930cc13
LuisSalazar02 <42986993+LuisSalazar02@users.noreply.github.com>
2018-09-25 18:17:24 -0500

    Add files via upload

---
+++ src/components/Header/Header.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+import "./Header.css";
+import Logo from "../Logo";
+import { PLAYLIST_ID } from "../../constants";
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div className="Header">
+        <div className="container">
+          <Logo />
+          <Link to={`/playlists/${PLAYLIST_ID}`} className="playlist">
+            My Playlist
+          </Link>
+        </div>
+      </div>
+    );
+  }
+}
